6f5adf55a6edac0773131e0e697ba9425c3a9c97
Marishka <marina.kozlova.spn@gmail.com>
2020-06-22 16:34:33 -0700

    new dbinfo parameters

--- dev/projects/php-students-database/dbinfo.php
+++ dev/projects/php-students-database/dbinfo.php
@@ -1,8 +1,8 @@
 <?php
 
-const DB_HOST	 = "";			
-const DB_USER	 = "msmirnov_bcit";	
-const DB_PASS	= "bcit-password";	
+const DB_HOST	 = "localhost";			
+const DB_USER	 = "msmirnov_a";	
+const DB_PASS	= "bcit-database";	
 const DB_NAME 	= "msmirnov_bcit";	
 	
 ?>
