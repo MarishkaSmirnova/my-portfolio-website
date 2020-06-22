e79f9e82d4025286004198d671dd8b4d23cd0c20
Marishka <marina.kozlova.spn@gmail.com>
2020-06-22 16:04:04 -0700

    dbinfo.php edit

--- dev/projects/php-students-database/dbinfo.php
+++ dev/projects/php-students-database/dbinfo.php
@@ -1,8 +1,8 @@
 <?php
 
 const DB_HOST	 = "localhost";			
-const DB_USER	 = "Marina";	
-const DB_PASS	= "12345";	
-const DB_NAME 	= "bcit";	
+const DB_USER	 = "msmirnov_bcit";	
+const DB_PASS	= "bcit-password";	
+const DB_NAME 	= "msmirnov_bcit";	
 	
 ?>
